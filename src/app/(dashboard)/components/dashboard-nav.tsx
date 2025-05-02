af193921b3f23ae2818f35b0d6dbd8e70894d75a
Ausath Ikram <ausathdzil@gmail.com>
2025-05-03 00:52:06 +0700

    Update sidebar style

---
+++ src/app/(dashboard)/components/dashboard-nav.tsx
@@ -0,0 +1,117 @@
+'use client';
+
+import {
+  LogOutIcon,
+  MoreVerticalIcon,
+  type LucideIcon
+} from 'lucide-react';
+
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from '@/components/ui/sidebar';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string;
+    url: string;
+    icon?: LucideIcon;
+  }[];
+}) {
+  const pathname = usePathname();
+
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent className="flex flex-col gap-2">
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={pathname === item.url}
+              >
+                <Link href={item.url}>
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+}
+
+export function NavUser({ user }: { user: { name: string; email: string } }) {
+  const { isMobile } = useSidebar();
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton size="lg">
+              <Avatar className="size-8 rounded-lg">
+                <AvatarFallback className="rounded-lg text-foreground">
+                  {user.name.charAt(0)}
+                </AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">{user.name}</span>
+                <span className="truncate text-xs text-muted">
+                  {user.email}
+                </span>
+              </div>
+              <MoreVerticalIcon className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            side={isMobile ? 'bottom' : 'right'}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="size-8 rounded-lg">
+                  <AvatarFallback className="rounded-lg">
+                    {user.name.charAt(0)}
+                  </AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="truncate text-xs text-muted-foreground">
+                    {user.email}
+                  </span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <LogOutIcon />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
--- src/app/(dashboard)/components/dashboard-sidebar.tsx
+++ src/app/(dashboard)/components/dashboard-sidebar.tsx
@@ -1,65 +1,80 @@
+'use client';
+
 import {
   ChartColumnIncreasingIcon,
   ClipboardCheckIcon,
   LayoutDashboardIcon,
+  SettingsIcon,
   UserRoundCogIcon,
 } from 'lucide-react';
 
 import {
   Sidebar,
   SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
+  SidebarFooter,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
-  SidebarMenuItem,
 } from '@/components/ui/sidebar';
+import Image from 'next/image';
 import Link from 'next/link';
+import { NavMain, NavUser } from './dashboard-nav';
 
-const items = [
-  {
-    title: 'Dashboard',
-    icon: LayoutDashboardIcon,
-    url: '/',
+const data = {
+  user: {
+    name: 'John Doe',
+    email: 'm@example.com',
   },
-  {
-    title: 'Analytics',
-    icon: ChartColumnIncreasingIcon,
-    url: '/analytics',
-  },
-  {
-    title: 'Cadre',
-    icon: UserRoundCogIcon,
-    url: '/cadre',
-  },
-  {
-    title: 'Metrics',
-    icon: ClipboardCheckIcon,
-    url: '/metrics',
-  },
-];
+  navMain: [
+    {
+      title: 'Dashboard',
+      icon: LayoutDashboardIcon,
+      url: '/',
+    },
+    {
+      title: 'Analytics',
+      icon: ChartColumnIncreasingIcon,
+      url: '/analytics',
+    },
+    {
+      title: 'Cadre',
+      icon: UserRoundCogIcon,
+      url: '/cadre',
+    },
+    {
+      title: 'Metrics',
+      icon: ClipboardCheckIcon,
+      url: '/metrics',
+    },
+  ],
+  navSecondary: [
+    {
+      title: 'Settings',
+      url: '#',
+      icon: SettingsIcon,
+    },
+  ],
+};
 
 export function DashboardSidebar() {
   return (
-    <Sidebar variant="floating" collapsible="icon">
+    <Sidebar variant="floating" collapsible="offcanvas">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuButton className="flex justify-center" asChild>
+            <Link href="/">
+              <Image src="/logo.svg" alt="logo" width={20} height={20} />
+              <span className="font-monomaniac-one text-xl">SILATIK</span>
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenu>
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        <NavMain items={data.navMain} />
       </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
     </Sidebar>
   );
 }
