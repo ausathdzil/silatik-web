37096f854312b815fe299d794b5d060d266c6249
Ausath Ikram <ausathdzil@gmail.com>
2025-05-03 12:30:46 +0700

    Update readme, force dynamic

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { DashboardSidebar } from './dashboard-sidebar';
 
+export const dynamic = 'force-dynamic';
+
 export default function DashboardLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
