37957f27cd2ea7a7fba7b51b5097ecf3951dcbc0
Ausath Ikram <ausathdzil@gmail.com>
2025-05-03 08:18:08 +0700

    Finish

--- src/app/(auth)/signin/signin-form.tsx
+++ src/app/(auth)/signin/signin-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -9,15 +11,22 @@ import {
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
 import Link from 'next/link';
-import { PasswordInput } from '../password-input';
+import { useState } from 'react';
 
 export function SignInForm() {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const toggleVisibility = () => setIsVisible((prevState) => !prevState);
+
   return (
     <Card>
       <CardHeader className="text-center">
         <CardTitle>Welcome Back!</CardTitle>
-        <CardDescription>Continue inspection and prevent DBD!</CardDescription>
+        <CardDescription>
+          Continue inspections and prevent Dengue!
+        </CardDescription>
       </CardHeader>
       <form>
         <CardContent>
@@ -27,21 +36,35 @@ export function SignInForm() {
               <Input
                 id="email"
                 type="email"
+                name="email"
                 placeholder="m@example.com"
                 required
               />
             </div>
             <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <a
-                  href="#"
-                  className="ml-auto text-sm underline-offset-4 hover:underline"
+              <Label htmlFor="password">Password</Label>
+              <div className="relative">
+                <Input
+                  id="password"
+                  name="password"
+                  className="pe-9"
+                  type={isVisible ? 'text' : 'password'}
+                />
+                <button
+                  className="text-muted-foreground/80 hover:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center rounded-e-md transition-[color,box-shadow] outline-none focus:z-10 focus-visible:ring-[3px] disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+                  type="button"
+                  onClick={toggleVisibility}
+                  aria-label={isVisible ? 'Hide password' : 'Show password'}
+                  aria-pressed={isVisible}
+                  aria-controls="password"
                 >
-                  Forgot password?
-                </a>
+                  {isVisible ? (
+                    <EyeOffIcon size={16} aria-hidden="true" />
+                  ) : (
+                    <EyeIcon size={16} aria-hidden="true" />
+                  )}
+                </button>
               </div>
-              <PasswordInput />
             </div>
           </div>
         </CardContent>
--- src/app/(auth)/signup/signup-form.tsx
+++ src/app/(auth)/signup/signup-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -9,52 +11,67 @@ import {
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
 import Link from 'next/link';
-import { PasswordInput } from '../password-input';
+import { useState } from 'react';
 
 export function SignUpForm() {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const toggleVisibility = () => setIsVisible((prevState) => !prevState);
+
   return (
     <Card>
       <CardHeader className="text-center">
-        <CardTitle>Create an Account</CardTitle>
-        <CardDescription>
-          Join us to start your inspection journey
-        </CardDescription>
+        <CardTitle>Join Jumantik!</CardTitle>
+        <CardDescription>Help prevent Dengue in your community</CardDescription>
       </CardHeader>
       <form>
         <CardContent>
           <div className="grid gap-6">
-            <div className="grid grid-cols-2 gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="firstName">First Name</Label>
-                <Input id="firstName" type="text" required />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="lastName">Last Name</Label>
-                <Input id="lastName" type="text" required />
-              </div>
+            <div className="grid gap-2">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                name="name"
+                type="text"
+                placeholder="John Doe"
+                required
+              />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
                 id="email"
+                name="email"
                 type="email"
                 placeholder="m@example.com"
                 required
               />
             </div>
             <div className="grid gap-2">
-              <Label htmlFor="telephone">Telephone</Label>
-              <Input
-                id="telephone"
-                type="tel"
-                placeholder="08123456789"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
-              <PasswordInput />
+              <div className="relative">
+                <Input
+                  id="password"
+                  name="password"
+                  className="pe-9"
+                  type={isVisible ? 'text' : 'password'}
+                />
+                <button
+                  className="text-muted-foreground/80 hover:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center rounded-e-md transition-[color,box-shadow] outline-none focus:z-10 focus-visible:ring-[3px] disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+                  type="button"
+                  onClick={toggleVisibility}
+                  aria-label={isVisible ? 'Hide password' : 'Show password'}
+                  aria-pressed={isVisible}
+                  aria-controls="password"
+                >
+                  {isVisible ? (
+                    <EyeOffIcon size={16} aria-hidden="true" />
+                  ) : (
+                    <EyeIcon size={16} aria-hidden="true" />
+                  )}
+                </button>
+              </div>
             </div>
           </div>
         </CardContent>
