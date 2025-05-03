9f8120b4911f971d36ae45d8772325a16bc23b4e
Ausath Ikram <ausathdzil@gmail.com>
2025-05-03 08:20:56 +0700

    Remove force-dynamic

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,8 +1,6 @@
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { DashboardSidebar } from './dashboard-sidebar';
 
-export const dynamic = 'force-dynamic';
-
 export default function DashboardLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
